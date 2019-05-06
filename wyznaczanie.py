bc99b056893fef8106e9dd120f31be58dc6def8a
MichalSz99 <m.szczepaniak99@gmail.com>
2019-05-06 21:27:32 +0200

    tworzenie grau

--- wyznaczanie.py
+++ wyznaczanie.py
@@ -19,9 +19,8 @@ class Edge:
 class GraphM:
 
     def __init__(self, n, g):
-        edge =  int(n*(n-1)*(g/100) )
-        if edge % 2 == 1:
-            edge +=1
+        MAXedge =  int(n*(n-1)*(g/100)/2 )
+        edge = 0
         self.matrix = []
         for i in range(n):
             self.matrix.append([])
@@ -32,7 +31,21 @@ class GraphM:
             self.matrix[i+1][i] = 1
         self.matrix[0][n-1] = 1
         self.matrix[n-1][0] = 1
-
+        edge = n
+        while edge < MAXedge:
+            x = random.randint(0,n-1)
+            y = random.randint(0,n-1)
+            z = random.randint(0,n-1)
+            if z==x or x==y or z==y or  self.matrix[x][y] == 1 or self.matrix[x][z] == 1 or self.matrix[y][z] == 1:
+                continue
+            else:
+                self.matrix[x][y] = 1
+                self.matrix[y][x] = 1
+                self.matrix[x][z] = 1
+                self.matrix[z][x] = 1
+                self.matrix[z][y] = 1
+                self.matrix[y][z] = 1
+                edge +=3
 
     def __str__(self):
         out = ""
@@ -48,7 +61,7 @@ class GraphM:
 
     def Hamilton(self,start):
         pass
-    
+
 matrix = GraphM(10, 30)
 print (matrix)
 
