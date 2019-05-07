fbe72f181a0e2ad9e8ad90c95686c29bfb1637d1
MichalSz99 <m.szczepaniak99@gmail.com>
2019-05-07 10:42:29 +0200

    Euler

--- wyznaczanie.py
+++ wyznaczanie.py
@@ -19,8 +19,6 @@ class Edge:
 class GraphM:
 
     def __init__(self, n, g):
-        MAXedge =  int(n*(n-1)*(g/100)/2 )
-        edge = 0
         self.matrix = []
         for i in range(n):
             self.matrix.append([])
@@ -31,6 +29,7 @@ class GraphM:
             self.matrix[i+1][i] = 1
         self.matrix[0][n-1] = 1
         self.matrix[n-1][0] = 1
+        MAXedge = round(n * (n - 1) * (g / 100) / 2)
         edge = n
         while edge < MAXedge:
             x = random.randint(0,n-1)
@@ -56,29 +55,60 @@ class GraphM:
             out = out + "\n" + pom
         return out
 
-    def Euler(self, start):
-        pass
+class GraphL:
+    def __init__(self, matrix):
+        self.list = []
+        l = len(matrix.matrix)
+        for i in range(l):
+            self.list.append([])
+            for j in range(l):
+                val = matrix.matrix[i][j]
+                if val != 0:
+                    self.list[i].append(j)
 
-    def Hamilton(self,start):
+    def __str__(self):
+        l = len(self.list)
+        out = ""
+        for i in range(l):
+            pom = str(i) + ":  "
+            li = len(self.list[i])
+            for j in range(li):
+                pom = pom + str(self.list[i][j]) + " "
+            out = out + "\n" + pom
+        return out
+    
+    def _Euler(self, out , v):
+        for i in self.list[v]:
+            self.list[v].remove(i) #tu moze sie wysypac
+            out = self._Euler(i)
+        out.append(v)
+        return out
+
+    def Euler(self, v):
+        out = []
+        stack = []
+        return self._Euler(out, stack, v)
+
+
+    def Hamilton(self,v):
         pass
 
 matrix = GraphM(10, 30)
 print (matrix)
-
+Lmatrix = GraphL(matrix)
+print(Lmatrix)
 
 """
-outM = open("PRIM70M.txt", 'w')
-outL = open("PRIM70L.txt", 'w')
-
+outM = open("Euler30.txt", 'w')
+outL = open("Hamilton30.txt", 'w')
 for i in range(1, 16):
     matrix = GraphM(500 * i, 70)
-
+    list = GraphL(matrix)
     startTime = time.time()
-    matrix.euler(0)
+    list.euler(0)
     endTime = time.time()
     Time = endTime - startTime
     outM.write("Euler" + str(Time) + "\n")
-
     startTime = time.time()
     list.hamilton(0)
     endTime = time.time()
